0e6d23a03206bc42e92ee2367a46b678552156dd
Vathanak <vathanaksol1605@gmail.com>
2024-11-06 15:03:59 +0700

    modify home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="app-container w-full h-screen flex justify-center items-center">
       <img 
         src="deploy.jpg"  
         alt="Deployment Success"
-        className="w-2/4 h-auto"  
+        className="w-1/4 h-auto rounded-2xl"  
       />
     </div>
   );
