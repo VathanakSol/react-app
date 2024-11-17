e11bb5cce1ab3bb9196bc32092adbb22450c4d6a
Vathanak <vathanaksol1605@gmail.com>
2024-11-17 22:45:09 +0700

    edit style in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Flow from "./Flow";
 function Home() {
   return (
     <div>
-      <div className="h-screen w-full flex justify-center items-center">
+      <div className="h-screen w-full flex justify-center items-center bg-green-400">
         <Flow />
       </div>
     </div>
